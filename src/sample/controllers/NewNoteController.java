79d044cd79d81ceb8356cea1677e3e904e3fafff
Ayodele <ayodele.glow@gmail.com>
2020-12-25 17:13:47 +0100

    initial-project-framework

---
+++ src/sample/controllers/NewNoteController.java
@@ -0,0 +1,17 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class NewNoteController {
+    //@FXML private Text actiontarget;
+
+    @FXML protected void handleSaveButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML protected void handleCancelButtonAction(ActionEvent event) {
+
+    }
+}
