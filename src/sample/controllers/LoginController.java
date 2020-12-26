a49d99c1a93435abf789e8c96d7c15d87673f359
Damilola Badmus <anuoluwa110401036@gmail.com>
2020-12-26 15:01:14 +0100

    first attempt at screen-linking

---
+++ src/sample/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    public void handleExitButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void handleLoginButtonAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
+        Parent root = FXMLLoader.load(getClass().getResource("../views/create-note.fxml"));
+        stage.setScene(new Scene(root, 300, 275));
+        stage.setTitle("Note Taking Application");
+        stage.show();
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
