ec10503320a83d81ea3570a8b1fe26377c4ce551
Audu Billy <audubilly89@gmail.com>
2020-12-29 19:10:19 +0100

    updated the project with Add Note Page

---
+++ src/sample/controllers/ListNotesController.java
@@ -0,0 +1,8 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+
+public class ListNotesController {
+    public void handleBAddNewNoteButtonAction(ActionEvent actionEvent) {
+    }
+}
--- src/sample/controllers/LoginController.java
+++ src/sample/controllers/LoginController.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,4 +29,5 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
 }
